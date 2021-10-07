40338acb0843c3cdbba3684b99722d2663077c32
Matt Dominey <matt.dominey@id.me>
2021-10-07 15:13:37 -0400

    asd

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 cd local/app
 echo "Running the thing: $PWD"
 ls -ahl
-/usr/bin/gem install bundle
+echo "Trying bundle"
+/usr/bin/gem install bundler
+echo "Tried bundle"
 bundle install --path vendor/bundle
 rails s
