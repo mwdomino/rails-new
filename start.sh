c1d0034caa97062e6e3f276b448f602398529873
Matt Dominey <matt.dominey@id.me>
2021-10-07 15:24:16 -0400

    asdasd

--- start.sh
+++ start.sh
@@ -5,5 +5,8 @@ ls -ahl
 echo "Trying bundle"
 /usr/bin/gem install bundler
 echo "Tried bundle"
+echo "Trying install"
 bundle install --path vendor/bundle
+echo "Tried install"
+echo "Trying rails s"
 rails s
