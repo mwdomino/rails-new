2871a0901ffd9ef4606d178d3a59445763c1a884
Matt Dominey <matt.dominey@id.me>
2021-10-08 13:59:37 -0400

    why

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 cd local/app
-echo "--------------"
-echo "Environment: "
-env
-echo "--------------"
-echo "Path: $PATH"
-echo "--------------"
 export HOME=local/app
+
 /usr/bin/gem install --user-install bundler 2>&1
+
 echo "--------------"
-echo "Avail gems:"
-ls -ahl $HOME/.gem/ruby/2.5.0/bin
+export PATH=$PATH:$HOME/.gem/ruby/2.5.0/bin/
+echo "PATH: $PATH"
+echo "--------------"
+
+echo "bundle -v"
+#echo "Avail gems:"
+#ls -ahl $HOME/.gem/ruby/2.5.0/bin
 #echo "running bundle install"
 #/nonexistent/.gem/ruby/2.5.0/bin/bundle install --path vendor/bundle 2>&1
 #echo "starting rails server"
