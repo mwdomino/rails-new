80982dbd6eb454afd5056ca79e0995e1b7107b27
Matt Dominey <matt.dominey@id.me>
2021-10-08 13:38:33 -0400

    add bugs

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd local/app
-echo "Running the thing: $PWD"
-echo "installing bundler"
-/usr/bin/gem install --user-install bundler 2>&1
-ls -ahl /nonexistent/.gem/ruby/2.5.0/bin
-echo "running bundle install"
-/nonexistent/.gem/ruby/2.5.0/bin/bundle install --path vendor/bundle 2>&1
-echo "starting rails server"
-/nonexistent/.gem/ruby/2.5.0/bin/rails s 2>&1
+echo "--------------"
+echo "Environment: "
+env
+echo "--------------"
+echo "Path: $PATH"
+echo "--------------"
+
+#/usr/bin/gem install --user-install bundler 2>&1
+#ls -ahl /nonexistent/.gem/ruby/2.5.0/bin
+#echo "running bundle install"
+#/nonexistent/.gem/ruby/2.5.0/bin/bundle install --path vendor/bundle 2>&1
+#echo "starting rails server"
+#/nonexistent/.gem/ruby/2.5.0/bin/rails s 2>&1
