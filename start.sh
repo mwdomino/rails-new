4d441c4cfa9970d69f5cfd714f8d19e982490125
Matt Dominey <matt.dominey@id.me>
2021-10-08 13:44:53 -0400

    asdf

--- start.sh
+++ start.sh
@@ -8,6 +8,8 @@ echo "Path: $PATH"
 echo "--------------"
 export HOME=local/app
 /usr/bin/gem install --user-install bundler 2>&1
+echo "--------------"
+echo "Avail gems:"
 ls -ahl $HOME/.gem/ruby/2.5.0/bin
 #echo "running bundle install"
 #/nonexistent/.gem/ruby/2.5.0/bin/bundle install --path vendor/bundle 2>&1
