c1ac1ba673907174393faa9ac4bcff53d3ab6a4f
Matt Dominey <matt.dominey@id.me>
2021-10-08 12:42:29 -0400

    anger

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd local/app
 echo "Running the thing: $PWD"
 ls -ahl
 echo "Trying bundle"
-/usr/bin/gem install bundler 2>&1
+/usr/bin/gem install --user-install bundler 2>&1
 echo "Tried bundle"
 echo "Trying install"
 bundle install --path vendor/bundle 2>&1
