c47a01779e9fa7c86ffeec7c859edd0e830dc638
Matt Dominey <matt.dominey@id.me>
2021-10-08 12:47:07 -0400

    argh

--- start.sh
+++ start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 cd local/app
 echo "Running the thing: $PWD"
-ls -ahl
-echo "Trying bundle"
+echo "installing bundler"
 /usr/bin/gem install --user-install bundler 2>&1
-echo "Tried bundle"
-echo "Trying install"
+echo "running bundle install"
 /nonexistent/.gem/ruby/2.5.0/bin/bundle install --path vendor/bundle 2>&1
-echo "Tried install"
-echo "Trying rails s"
-rails s 2>&1
+echo "starting rails server"
+/nonexistent/.gem/ruby/2.5.0/bin/rails s 2>&1
