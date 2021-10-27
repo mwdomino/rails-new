48dee29843e9b3aa4d343a96c0740d874480eaeb
Matt Dominey <matt.dominey@id.me>
2021-10-27 11:49:00 -0400

    full send

--- start.sh
+++ start.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 cd local/app
+export TERM=xterm # ctrl+l is very important to me
 export HOME=$PWD
-echo "The cow says: $(whoami):$(groups)"
-chown -R $(whoami) $HOME
-/usr/bin/gem install --user-install bundler 2>&1
+export PATH=$HOME/vendor/bundle/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-echo "--------------"
-export PATH=$PATH:$HOME/.gem/ruby/2.5.0/bin/
-echo "PATH: $PATH"
-echo "--------------"
+# Bundle options, can be configured either with bundle config set or ENV
+export BUNDLE_BIN=$HOME/bin
+export BUNDLE_DEPLOYMENT=true
+export BUNDLE_PATH=$HOME/vendor/bundle
 
-echo "bundle -v"
-#echo "Avail gems:"
-#ls -ahl $HOME/.gem/ruby/2.5.0/bin
-#echo "running bundle install"
-#/nonexistent/.gem/ruby/2.5.0/bin/bundle install --path vendor/bundle 2>&1
-#echo "starting rails server"
-#/nonexistent/.gem/ruby/2.5.0/bin/rails s 2>&1
+bundle install
+bundle exec rails webpacker:install
+bundle exec rails s
