dec4cd399bf3356a6d33b370bdf33d47f7930cf3
Matt Dominey <matt.dominey@id.me>
2021-10-27 14:10:44 -0400

    please work :(

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /alloc/data 
 export TERM=xterm # ctrl+l is very important to me
-# export HOME=$PWD
+export HOME=$PWD
 export PATH=$HOME/.gem/ruby/2.5.0/bin:$HOME/vendor/bundle/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Bundle options, can be configured either with bundle config set or ENV
