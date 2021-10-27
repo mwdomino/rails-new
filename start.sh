044ef07bd4da7fe8785eed523a8df650d3100f7d
Matt Dominey <matt.dominey@id.me>
2021-10-27 13:11:58 -0400

    please work :(

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-cd local/app
 export TERM=xterm # ctrl+l is very important to me
 # export HOME=$PWD
 export PATH=$HOME/.gem/ruby/2.5.0/bin:$HOME/vendor/bundle/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Bundle options, can be configured either with bundle config set or ENV
-export BUNDLE_BIN=$HOME/bin
+export BUNDLE_BIN=/local/app/bin
 export BUNDLE_DEPLOYMENT=true
-export BUNDLE_PATH=$HOME/vendor/bundle
+export BUNDLE_PATH=/local/app/vendor/bundle
 
 gem install --user-install bundler 2>&1
-bundle install 2>&1
+/usr/local/bin/bundle install 2>&1
 
-bundle exec rails webpacker:install 2>&1
+/usr/local/bin/bundle exec rails webpacker:install 2>&1
 
-bundle exec rails s 2>&1
+/usr/local/bin/bundle exec rails s 2>&1
