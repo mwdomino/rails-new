6580c81c8e2c5a6889ed4953c84d0536a40172f4
Matt Dominey <matt.dominey@id.me>
2021-10-27 13:00:00 -0400

    asfljdsflk

--- start.sh
+++ start.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-cd local/app
 export TERM=xterm # ctrl+l is very important to me
 # export HOME=$PWD
-export PATH=$HOME/node_modules/.bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/vendor/bundle/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH=$HOME/.gem/ruby/2.5.0/bin:$HOME/vendor/bundle/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 # Bundle options, can be configured either with bundle config set or ENV
 export BUNDLE_BIN=$HOME/bin
 export BUNDLE_DEPLOYMENT=true
 export BUNDLE_PATH=$HOME/vendor/bundle
 
-sleep 9000
 gem install --user-install bundler 2>&1
-npm install yarn 2>&1 
-/usr/local/bin/bundle install 2>&1
+bundle install 2>&1
 
-/usr/local/bin/bundle exec rails webpacker:install 2>&1
+bundle exec rails webpacker:install 2>&1
 
-/usr/local/bin/bundle exec rails s 2>&1
+bundle exec rails s 2>&1
